b70495adc24c4a67380e094985442a6c1682720f
nikhildinnu <nikhil.dinnu@gmail.com>
2024-06-28 23:17:07 -0500

    Model tranning

--- src/utils.py
+++ src/utils.py
@@ -21,4 +21,40 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+def evaluate_models(X_train,Y_train,X_test,Y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,Y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,Y_train)
+
+            Y_train_pred = model.predict(X_train)
+
+            Y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(Y_train, Y_train_pred)
+
+            test_model_score = r2_score(Y_test, Y_test_pred)
+
+            report[list(models.key())[i]] = test_model_score
+
+        return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
